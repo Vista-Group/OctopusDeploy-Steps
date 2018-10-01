2edd482137a852eddf6da546e23178832bb2c2d0
Ben Clapp <Ben.Clapp@vista.co>
2018-10-02 12:24:06 +1300

    commit Gerard's changes from Octopus to this repo

--- kubernetes/apply.sh
+++ kubernetes/apply.sh
@@ -17,6 +17,9 @@ release_number=$(get_octopusvariable "Octopus.Release.Number")
 PackageRoot=$HOME/.octopus/OctopusServer/Work/tools/$release_number/$namespace
 echo "Using PackageTransferPath: $PackageRoot"
 
+ConfigurationFilename=$(get_octopusvariable "__CONFIGURATION_FILENAME")
+echo "Render Kubernetes Config $ConfigurationFilename for stack $stack"
+
 # If the ingress is a multi cluster deployment
 # for example: ../ci/.. vs ../production/mx-test/..
 echo "The deployment has particular config for each cluster: $multiClusterDeployment"
@@ -28,47 +31,60 @@ else #Otherwise, do the standard single cluster deployment
     echo "Using standard single cluster deployment: $envDir"
 fi
 
-infraVariables=$PackageRoot/environments/$envDir/k8s-infrastructure.yaml
-echo "Using ifraVariables file: $infraVariables"
+#Hack for the egress stack (inspects the load balancer ip with azureRM)
+KeyValuePairs=$(get_octopusvariable "__KEY_VALUE_PAIRS")
+echo "Extra Config KeyValuePairs $KeyValuePairs"
 
-echo "vista_deployer: $deployer" >> $infraVariables
-echo "vista_deployment_date: $deployment_date" >> $infraVariables
-echo "vista_deployment_id: $deployment_id" >> $infraVariables
-echo "vista_deployment_name: $deployment_name" >> $infraVariables
-echo "vista_release_id: $release_id" >> $infraVariables
-echo "vista_release_number: $release_number" >> $infraVariables
-
+#Create the metadata with the image tag version
 echo "namespace=$namespace" > $namespace.env
 echo >> $namespace.env
 
 #Avoid the warning from no matching files
 shopt -u nullglob
+find $PackageRoot -maxdepth 1 -type f -name '*.env'
 filecount=$(find $PackageRoot -maxdepth 1 -type f -name '*.env' | wc -l)
 echo "Number of files in the directory: $filecount"
-if [ $filecount \> 0 ]; then
-        for FILE in $PackageRoot/*.env; do
-            echo "Processing env filename $FILE"
-            #hack to remove the export keywork as we don't use config from env anymore
-            cat $FILE | awk  '{ print $2 }' >> $namespace.env
-            echo >> $namespace.env
-        done
+if [ $filecount == '0' ]; then
+	echo "No env files to process"
 else
-        echo "No env files to process"
+  for FILE in $PackageRoot/*.env; do
+    echo "Processing env filename $FILE"
+    #hack to remove the export keywork as we don't use config from env anymore
+    cat $FILE | awk  '{ print $2 }' >> $namespace.env
+    echo >> $namespace.env
+  done
 fi
 
+
 #Invoke Kubernetes CLI for this particular environment
-echo
-echo "Running Kubectl CLI compose for $namespace with following properties file content:"
+echo "Adding deployment metadata to infrastructure settings file"
+echo "vista_deployer: $deployer" >> $namespace.env
+echo "vista_deployment_date: $deployment_date" >> $namespace.env
+echo "vista_deployment_id: $deployment_id" >> $namespace.env
+echo "vista_deployment_name: $deployment_name" >> $namespace.env
+echo "vista_release_id: $release_id" >> $namespace.env
+echo "vista_release_number: $release_number" >> $namespace.env
+
+echo "Running Kubectl CLI compose for $namespace with image and deployment metadata content:"
 cat $namespace.env
-echo "Running Kubectl CLI compose with following infrastructure settings file content:"
-cat $infraVariables
 
-KeyValuePairs=$(get_octopusvariable "__KEY_VALUE_PAIRS")
-echo "Extra Config KeyValuePairs $KeyValuePairs"
+#Render using infraVariables only if available
+infraVariables=$PackageRoot/environments/$envDir/k8s-infrastructure.yaml
+echo "Using ifraVariables file: $infraVariables (if available)"
 
-ConfigurationFilename=$(get_octopusvariable "__CONFIGURATION_FILENAME")
-echo "Render Config $ConfigurationFilename for stack $stack"
-kubetpl render $PackageRoot/k8s/$stack/$ConfigurationFilename -i $namespace.env -i $infraVariables -s $KeyValuePairs | cat -
+if [ ! -f $infraVariables ]; then
+    echo "Running Kubectl CLI compose without infrastructure settings file available!"
+	kubetpl render $PackageRoot/k8s/$stack/$ConfigurationFilename --debug -i $namespace.env -s $KeyValuePairs > $PackageRoot/rendered-$namespace-$stack-deployment.yaml    
+else
+	echo "Running Kubectl CLI compose with following infrastructure settings file content:"
+	cat $infraVariables
+
+	kubetpl render $PackageRoot/k8s/$stack/$ConfigurationFilename --debug -i $namespace.env -i $infraVariables -s $KeyValuePairs > $PackageRoot/rendered-$namespace-$stack-deployment.yaml
+fi
+
+#publish the rendered deployment as an artifact path and name
+echo "Publish (print) Config $ConfigurationFilename for stack $stack"
+cat $PackageRoot/rendered-$namespace-$stack-deployment.yaml
 
 echo "Apply Config $ConfigurationFilename for stack $stack"
-kubetpl render $PackageRoot/k8s/$stack/$ConfigurationFilename -i $namespace.env -i $infraVariables -s $KeyValuePairs | kubectl --context=$context apply -f -
+kubectl --context=$context apply -f $PackageRoot/rendered-$namespace-$stack-deployment.yaml
