023377e02d023c88b327e1b0f40d9f96b2cbc14b
Vista Group <34108958+Vista-Group@users.noreply.github.com>
2018-10-17 15:12:30 +1300

    Update deploy-tls-secret.sh

--- kubernetes/deploy-tls-secret.sh
+++ kubernetes/deploy-tls-secret.sh
@@ -3,7 +3,8 @@
 release_number=$(get_octopusvariable "Octopus.Release.Number")
 namespace=$(get_octopusvariable "__NAMESPACE")
 context=$(get_octopusvariable "__CONTEXT")
-secret_name="traefik-tls-cert"
+# defaults to "traefik-tls-cert"
+secret_name=$(get_octopusvariable "__SECRET_NAME")
 
 # Get the configuration (docker compose and environ overrides) from the OctoPackage 'gtp_config'
 # Resolve docker templates for a particular stack from manifest
@@ -17,20 +18,19 @@ if [ "$?" = "0" ]; then
     echo "TLS Secret $secret_name already exists in $namespace"
 else
     echo "TLS Secret $secret_name does not currently exist in $namespace: Adding..."
-    
     cd $PackageRoot
 
-	# This assumes the Octopus certificate was created 
-	# from a PEM Cert + PrivateKey
-	CERT=$(get_octopusvariable "OriginCertificate")
-	echo "Cert Name (Octopus Library): $CERT"
+    # This assumes the Octopus certificate was created 
+    # from a PEM Cert + PrivateKey
+    CERT=$(get_octopusvariable "__CERTIFICATE")
+    echo "Cert Name (Octopus Library): $CERT"
 
-    KEY=$(get_octopusvariable "OriginCertificate.PrivateKeyPem")
+    KEY=$(get_octopusvariable "__CERTIFICATE.PrivateKeyPem")
     echo "$KEY" > $PackageRoot/tls.key
-	# check private key with openssl
-	openssl rsa -in $PackageRoot/tls.key -check
+    # check private key with openssl
+    openssl rsa -in $PackageRoot/tls.key -check
 
-    CERT=$(get_octopusvariable "OriginCertificate.CertificatePem")
+    CERT=$(get_octopusvariable "__CERTIFICATE.CertificatePem")
     echo "$CERT" > $PackageRoot/tls.crt
     # check certificate with openssl
     openssl x509 -in $PackageRoot/tls.crt -text -noout
