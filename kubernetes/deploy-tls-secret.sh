ea41cbbaae987a1da2ef0dfe7c206f5645508178
Vista Group <34108958+Vista-Group@users.noreply.github.com>
2018-11-09 17:38:53 +1300

    add full chain of certs

--- kubernetes/deploy-tls-secret.sh
+++ kubernetes/deploy-tls-secret.sh
@@ -42,9 +42,14 @@ else
     openssl x509 -in $PackageRoot/cacerts/DigiCert_High_Assurance_EV_Root_CA.crt -text -noout
     openssl x509 -in $PackageRoot/cacerts/DigiCert_SHA2_High_Assurance_Server_CA.crt -text -noout
 
-    cat $PackageRoot/cacerts/DigiCert_High_Assurance_EV_Root_CA.crt >> $PackageRoot/tls.crt
-    cat $PackageRoot/cacerts/DigiCert_SHA2_High_Assurance_Server_CA.crt >> $PackageRoot/tls.crt
+    traffic_type=$(get_octopusvariable "__TRAFFIC_TYPE")
+    if ["$traffic_type" == "api"]
+    then
+        echo "adding the full chain of certs"
+        cat $PackageRoot/cacerts/DigiCert_High_Assurance_EV_Root_CA.crt >> $PackageRoot/tls.crt
+        cat $PackageRoot/cacerts/DigiCert_SHA2_High_Assurance_Server_CA.crt >> $PackageRoot/tls.crt
+    fi
     
     kubectl --context=$context create secret tls "$secret_name" --cert=$PackageRoot/tls.crt --key=$PackageRoot/tls.key --namespace "$namespace"
-    kubectl --context=$context label secret "$secret_name" traffic-type=api -n "$namespace"
+    kubectl --context=$context label secret "$secret_name" traffic-type="$traffic_type" -n "$namespace"
 fi
